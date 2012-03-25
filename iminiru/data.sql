d0d763788d0ed76335b8282ce719ec0a2cff246a
Gergely POLONKAI <polesz@w00d5t0ck.info>
2012-03-25 11:56:40 +0200

    Added the very basic dump of the world Iminiru.
    Signed-off-by: Gergely POLONKAI <polesz@w00d5t0ck.info>

---
+++ iminiru/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO directions (id, short_name, name) VALUES (1, 'n', 'north');
+INSERT INTO directions (id, short_name, name) VALUES (2, 'nw', 'northwest');
+INSERT INTO directions (id, short_name, name) VALUES (3, 'w', 'west');
+INSERT INTO directions (id, short_name, name) VALUES (4, 'sw', 'southwest');
+INSERT INTO directions (id, short_name, name) VALUES (5, 's', 'south');
+INSERT INTO directions (id, short_name, name) VALUES (6, 'se', 'southeast');
+INSERT INTO directions (id, short_name, name) VALUES (7, 'e', 'east');
+INSERT INTO directions (id, short_name, name) VALUES (8, 'ne', 'northeast');
+INSERT INTO directions (id, short_name, name) VALUES (9, 'u', 'up');
+INSERT INTO directions (id, short_name, name) VALUES (10, 'd', 'down');
+
+INSERT INTO planes (id, name) VALUES (1, 'Erodar');
+INSERT INTO planes (id, name) VALUES (2, 'Indiar');
+INSERT INTO planes (id, name) VALUES (3, 'Zothar');
+
+INSERT INTO planets (id, name) VALUES (1, 'Erodar');
+
+INSERT INTO planet_planes (1, 1);
+INSERT INTO planet_planes (1, 2);
+INSERT INTO planet_planes (1, 3);
+
+INSERT INTO areas (id, name) VALUES (1, 'Tutorial Area');
+
+INSERT INTO rooms (id, area, name, distant_description, close_description) VALUES (1, 1, 'Tutorial Welcome Area', 'You see a small room with some tables and chairs in it.', 'You are in a small room with some tables and chairs.');
+INSERT INTO rooms (id, area, name, distant_description, close_description) VALUES (2, 1, 'Tutorial Fighting Area', 'You see a small arena barely enough for a few people to fight.', 'You are in a small arena. It is barely large enough for few people to fight.');
+
+INSERT INTO room_exits (room_id, direction, other_side) VALUES (1, 1, 2);
+INSERT INTO room_exits (room_id, direction, other_side) VALUES (2, 5, 1);
