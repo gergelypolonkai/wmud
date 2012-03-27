92ff91b9896e0319dea52b250424938a780339d6
Polonkai Gergely <polesz@w00d5t0ck.info>
2012-03-27 19:39:47 +0000

    Made create-world.sh and drop-world.sh a template.
    create-world.sh and drop-world.sh are now autogenerated by the configure
    script, as they need to hold some paths set by that script.
    
    Signed-off by Gergely POLONKAI <polesz@w00d5t0ck.info>

--- create-world.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-sqlite3 iminiru.db < sql/planes.sql
-sqlite3 iminiru.db < sql/planets.sql
-sqlite3 iminiru.db < sql/rooms.sql
-
-sqlite3 iminiru.db < iminiru/data.sql
-
--- drop-world.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-sqlite3 iminiru.db < sql/drop-world.sql
