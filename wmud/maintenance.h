bc3eab6b2834b1ee72b919c53f4dff7923cd1cf6
Gergely Polonkai (W00d5t0ck) <polesz@w00d5t0ck.info>
2012-03-29 17:58:58 +0200

    Added the self-standard comments to maintenance.h
    Signed-off-by: Gergely Polonkai (W00d5t0ck) <polesz@w00d5t0ck.info>

--- wmud/maintenance.h
+++ wmud/maintenance.h
@@ -1,1 +1,25 @@
+/* wMUD - Yet another MUD codebase by W00d5t0ck
+ * Copyright (C) 2012 - Gergely POLONKAI
+ *
+ * maintenance.c: game maintenance and self-check functions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __WMUD_MAINTENANCE_H__
+#define __WMUD_MAINTENANCE_H__
+
 void wmud_maintenance_init(void);
+
+#endif /* __WMUD_MAINTENANCE_H__ */
+
