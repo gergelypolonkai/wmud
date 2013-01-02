50a487b7e22d0b2a1b46eb6b6161bf5a6523f5e3
Gergely Polonkai (W00d5t0ck) <polesz@w00d5t0ck.info>
2013-01-02 20:01:42 +0100

    Removed the GIO SMTP startup
    It is obsoleted in favor of libcurl

--- libgiosmtp/giomodule.c
+++
@@ -1,4 +0,0 @@
-#include <gio/gio.h>
-#include <curl/curl.h>
-//G_DEFINE_TYPE(ApplicationProtocolIface, application_protocol_iface, APPLICATION_PROTOCOL_INTERFACE);
-//g_io_extension_point_implement("application-protocol", application_protocol_iface_get_type(), "smtp", 10);
